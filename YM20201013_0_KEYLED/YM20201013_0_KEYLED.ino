8fe33143f7c0a409bcadf470fe635fd3b28fadd9
TIK <firmament_solo@hotmail.com>
2020-10-13 12:01:28 +0800

    multi-output sample

--- YM20201013_0_KEYLED/YM20201013_0_KEYLED.ino
+++ YM20201013_0_KEYLED/YM20201013_0_KEYLED.ino
@@ -1,13 +1,20 @@
+//0CT 13 2020 T.I. multi-output sample
+//OCT 13 2020 T.I. created this file.
+
 int key_0=3;
 int key_1=4;
 int key_2=5;
 
 int led_0=11;
+int led_1=12;
+int led_2=13;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(key_0,INPUT_PULLUP);
   pinMode(led_0,OUTPUT);
+  pinMode(led_1,OUTPUT);
+  pinMode(led_2,OUTPUT);
 }
 
 void loop() {
@@ -16,10 +23,14 @@ void loop() {
   if(LOW==digitalRead(key_0))
   {
      digitalWrite(led_0,HIGH);
+     digitalWrite(led_1,HIGH);
+     digitalWrite(led_2,HIGH);
   }
   else
   {
      digitalWrite(led_0,LOW);
+     digitalWrite(led_1,LOW);
+     digitalWrite(led_2,LOW);
   }
   
 }
